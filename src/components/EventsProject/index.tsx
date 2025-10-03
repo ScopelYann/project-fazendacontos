e0a9064d079feeb1369c707d411c698f5326a685
Scopel_Yan <160073076+ScopelYann@users.noreply.github.com>
2025-10-03 04:24:24 -0300

    Carrossel-ok

--- src/components/EventsProject/index.tsx
+++ src/components/EventsProject/index.tsx
@@ -1,32 +1,49 @@
-import React from "react";
-import { InfosComponent, MainWraper } from "./style";
+import React, { useRef, useState } from "react";
+import { InfosComponent, MainWraper, CustomSwiperButton } from "./style";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { FreeMode, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export const CardsComponents: React.FC = () => {
+  const [swiperInstance, setSwiperInstance] = useState<any>(null);
+  const [isBeginning, setIsBeginning] = useState<boolean>(true);
+  const [isEnd, setIsEnd] = useState<boolean>(false);
+
   const experienceInFazenda = [
-    { id: 0, name: "Peças de teatro" },
-    { id: 1, name: "Passeio na natureza" },
-    { id: 2, name: "Atividades de socialização e recreação" },
-    { id: 3, name: "Cinema ao ar livre" },
-    { id: 4, name: "Jogos interativos" },
-    { id: 5, name: "Música ao vivo" },
+    { id: 0, name: "1" },
+    { id: 1, name: "2" },
+    { id: 2, name: "3" },
+    { id: 3, name: "4" },
+    { id: 4, name: "5" },
+    { id: 5, name: "6" },
+    { id: 6, name: "7" },
+    { id: 7, name: "8" },
+    { id: 8, name: "9" },
   ];
 
   return (
     <MainWraper>
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Pagination, Navigation, FreeMode]}
         spaceBetween={100}
         slidesPerView={3}
-        slidesPerGroup={1}
+        slidesPerGroup={3}
+        freeMode={true}
+        speed={50}
         centeredSlides={false}
-        navigation
         pagination={{ clickable: true }}
         loop={false}
+        onSwiper={(swiper) => {
+          setSwiperInstance(swiper);
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
+        onSlideChange={(swiper) => {
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
+        }}
         className="swiper"
       >
         {experienceInFazenda.map((item) => (
@@ -37,6 +54,68 @@ export const CardsComponents: React.FC = () => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <CustomSwiperButton
+        verifbullet={isBeginning ? 1 : 0}
+        className="custom-prev"
+        onClick={() => {
+          swiperInstance.slidePrev();
+        }}
+      >
+        <svg
+          fill="#ffffff"
+          version="1.1"
+          id="Capa_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 199.404 199.404"
+          xmlSpace="preserve"
+          stroke="#ffffff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <g>
+              {" "}
+              <polygon points="135.412,0 35.709,99.702 135.412,199.404 163.695,171.119 92.277,99.702 163.695,28.285 "></polygon>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+      </CustomSwiperButton>
+      <CustomSwiperButton
+        verifbullet={isEnd ? 1 : 0}
+        className="custom-next swiper-button-disabled"
+        onClick={() => {
+          swiperInstance.slideNext();
+        }}
+      >
+        <svg
+          fill="#ffffff"
+          version="1.1"
+          id="Capa_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 0 199.404 199.404"
+          xmlSpace="preserve"
+          stroke="#ffffff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            <g>
+              {" "}
+              <polygon points="135.412,0 35.709,99.702 135.412,199.404 163.695,171.119 92.277,99.702 163.695,28.285 "></polygon>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+      </CustomSwiperButton>
     </MainWraper>
   );
 };
--- src/components/EventsProject/style.ts
+++ src/components/EventsProject/style.ts
@@ -5,20 +5,34 @@ export const MainWraper = styled.div`
   display: flex;
   justify-content: center;
   padding: 20px 0;
- 
+  .custom-prev {
+    position: absolute;
+    left: 2%;
+    top: 45%;
+    width: 37px;
+  }
+
+  .custom-next {
+    position: absolute;
+    left: 95%;
+    top: 45%;
+    width: 37px;
+    transform: rotate(180deg);
+  }
+
   .swiper {
     max-width: 1140px; /* Largura exata para 3 cards de 360px + espaçamentos */
     width: 100%;
     overflow: hidden;
     padding: 0 50px; /* Espaço para os botões de navegação */
   }
-  
+
   .swiper-slide {
     display: flex;
     justify-content: center;
     box-sizing: border-box;
   }
-  
+
   .swiper-wrapper {
     align-items: center;
   }
@@ -38,6 +52,10 @@ export const InfosComponent = styled.div`
   transition: transform 0.2s ease;
   position: relative;
   flex-shrink: 0; /* Impede que os cards encolham */
-  
+`;
 
+export const CustomSwiperButton = styled.div<{ verifbullet: number }>`
+  opacity: ${(props) => (props.verifbullet == 1 ? 0.5 : 1)};
+  cursor: ${props => props.verifbullet == 1 ? 'not-allowed' : 'pointer'};
+  transition: all 1s;
 `;
